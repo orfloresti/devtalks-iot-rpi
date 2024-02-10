1d74048b3e8cbac4f6637ee633e0fc41b05825f8
Orlando Flores Teomitzi <orlando.floresti@gmail.com>
2024-02-10 01:25:56 -0600

    Add code for arduino and update for dockerfile to use forward port 1883

--- iot-device/iot-device.ino
+++ iot-device/iot-device.ino
@@ -1,9 +1,86 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
+ Basic MQTT example
 
+ This sketch demonstrates the basic capabilities of the library.
+ It connects to an MQTT server then:
+  - publishes "hello world" to the topic "outTopic"
+  - subscribes to the topic "inTopic", printing out any messages
+    it receives. NB - it assumes the received payloads are strings not binary
+
+ It will reconnect to the server if the connection is lost using a blocking
+ reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
+ achieve the same result without blocking the main loop.
+ 
+*/
+
+#include <SPI.h>
+#include <Ethernet.h>
+#include <PubSubClient.h>
+
+// Update these with values suitable for your network.
+byte mac[] = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
+IPAddress ip(192,168,1,50);
+IPAddress server(192,168,1,67);
+
+EthernetClient ethClient;
+PubSubClient client(ethClient);
+
+int light = 7;
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  
+  if( (char)payload[0] == '1' ) {
+    Serial.print("Light turn on");
+    digitalWrite(light, HIGH);
+  }
+
+  if( (char)payload[0] == '0' ) {
+    Serial.print("Light turn off");
+    digitalWrite(light, LOW);
+  }
+
+  Serial.println();
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("arduinoClient")) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("/home/light", "Hello from device!");
+      // ... and resubscribe
+      client.subscribe("/home/light");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(light, OUTPUT);
+
+  client.setServer(server, 1883);
+  client.setCallback(callback);
+
+  Ethernet.begin(mac, ip);
+  // Allow the hardware to sort itself out
+  delay(1500);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
 }
